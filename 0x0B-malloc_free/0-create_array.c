76350fc0ab6bf19ffdddcd894ea553b1dd9cbc56
zel-tes <zeltes18@gmail.com>
2023-10-30 10:51:57 -0700

    2nd commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,20 +9,20 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-        unsigned int i;
-        char *str;
+	unsigned int i;
+	char *str;
 
-        if (size == 0)
+	if (size == 0)
 		return (NULL);
 	str = malloc(size * sizeof(char));
-        if (str == NULL)
-        {
-                return NULL;
-        }
-        for ( i = 0; i < size; i++)
-        {
-                str[i] = c;
-        }
-        return str;
-        free(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+	free(str);
 }
