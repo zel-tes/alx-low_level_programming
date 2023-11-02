1fa73e8a19c03e7bfdad61834aef2d28135ecca0
zel-tes <zeltes18@gmail.com>
2023-11-02 13:41:52 -0700

    10th commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - prints buffer in hexa
+ * @ptr: the address of memory to print
+ * @old_size: the size of the memory to print
+ * @new_size: updated size
+ *
+ * Return: Nothing.
+ **/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *nptr;
+	char *str, *nstr;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	str = (char *)ptr;
+	nptr = (void *)malloc(new_size);
+	nstr = (char *)nptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			nstr[i] = str[i];
+		}
+	}
+	return (nstr);
+	free(ptr);
+	free(nptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,4 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int multiply(int *num1, int *num2);
-void simple_print_buffer(int *buffer, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size);
