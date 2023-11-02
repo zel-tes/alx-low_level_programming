be0a93e93e67613e7bb206ee3d614a5cd60b0610
zel-tes <zeltes18@gmail.com>
2023-11-01 20:20:49 -0700

    3rd chance commit @1

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,23 +19,16 @@ int **alloc_grid(int width, int height)
 	}
 	array = (int **)malloc(height * sizeof(int *));
 	if (array == NULL)
-	{
-		return (NULL);
 		free(array);
-	}
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(width * sizeof(int));
 
 		if (array[i] == NULL)
-		{
-			return (NULL);
 			free(array);
-		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 	return (array);
-	free(array);
 }
