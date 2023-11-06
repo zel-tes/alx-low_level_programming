aa75ed91b6b765c322692ad9e45f5b7ec11bc988
zel-tes <zeltes18@gmail.com>
2023-11-06 10:03:46 -0800

    2nd commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	struct dog *p = d;
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
 }
