00dd3db5b1b8b5d33391acc860f8915c55f31f67
zel-tes <zeltes18@gmail.com>
2023-11-06 20:47:35 -0800

    2nd chance commit@3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,8 @@ void print_dog(struct dog *d)
 		exit(1);
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	if (d->age == .0f)
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
