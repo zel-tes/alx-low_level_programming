a9d6280c68c0c1b34acf7d7367188a056665f1b1
zel-tes <zeltes18@gmail.com>
2023-11-08 11:46:16 -0800

    3rd commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - prints a name as is
  * @name: name of the person
@@ -9,5 +9,7 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		exit(1);
 	f(name);
 }
