4e397761faa6a73a77b712e8782544bb104ebff4
zel-tes <zeltes18@gmail.com>
2023-11-08 15:57:22 -0800

    9th commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * array_iterator - prints an integer
  * @array: the integer to print
@@ -12,6 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long i;
 
+	if (size == 0 || array == NULL || action == NULL)
+		exit(1);
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
