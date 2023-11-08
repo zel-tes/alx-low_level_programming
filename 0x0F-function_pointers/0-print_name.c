eab45bf19a3ab268f7a942cc8bce0984d0e6196c
zel-tes <zeltes18@gmail.com>
2023-11-08 11:35:22 -0800

    1st commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function pointer
+ *
+ * Return: Nothing.
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
