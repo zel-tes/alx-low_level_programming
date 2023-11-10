784d75a52521ddfc116291d41b9654c73f728e1a
zel-tes <zeltes18@gmail.com>
2023-11-09 16:15:20 -0800

    3rd commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,10 +19,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 	{
+		i = 0;
 		while (i < n)
 		{
+
 			printf("%s", va_arg(ap, char *));
-			i++;
+			i += 2;
 		}
 	}
 	for (i = 0; i < n - 1; i++)
