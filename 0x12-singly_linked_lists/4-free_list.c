550a52418c633551d81dfbcbe2de3a625db88bc9
zel-tes <zeltes18@gmail.com>
2023-12-23 06:55:58 -0800

    another one

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees memory
+ * @head: list pointer
+ *
+ * Return: Always void.
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+	while(head != NULL)
+	{
+		node = head;
+		free(node->str);
+		head = head->next;
+
+		free(node);
+	}
+}
